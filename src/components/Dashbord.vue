014df55679152f394631d2e38afd93305118ee8d
alexzhang <alexzhangyaotian@gmail.com>
2023-05-29 13:43:35 +0800

    modify:props 验证

--- src/components/Dashbord.vue
+++ src/components/Dashbord.vue
@@ -1,11 +1,21 @@
 <template>
+  <!-- 下次节点更新 -->
   <NextTickT></NextTickT>
+  <!-- 传送门 -->
   <TeleportT></TeleportT>
+  <!-- 生命周期钩子 -->
   <LifeHooks></LifeHooks>
+  <!-- 动态css -->
   <DynamicCss></DynamicCss>
+  <!-- ref全家桶 -->
   <RefFamily></RefFamily>
+  <!-- 阻止冒泡 -->
   <PreventProp></PreventProp>
+  <!-- v-model自定义修饰符 -->
   <ModelCapitalize></ModelCapitalize>
+  <!-- Btn prop类型验证 -->
+  <ValidateProp btn-type="success"></ValidateProp>
+
 </template>
 <script setup>
 import NextTickT from "./NextTickT.vue";
@@ -15,6 +25,7 @@ import DynamicCss from "./DynamicCss.vue";
 import RefFamily from "./RefFamily.vue";
 import PreventProp from "./PreventProp.vue";
 import ModelCapitalize from "./ModelCapitalize.vue";
+import ValidateProp from "./ValidateProp.vue";
 import { defineComponent } from "vue";
 defineComponent({
   NextTickT,
@@ -23,6 +34,7 @@ defineComponent({
   DynamicCss,
   RefFamily,
   PreventProp,
-  ModelCapitalize
+  ModelCapitalize,
+  ValidateProp
 });
 </script>
---
+++ src/components/ValidateProp.vue
@@ -0,0 +1,14 @@
+<template>
+  <button>Button</button>
+</template>
+<script setup>
+defineProps({
+  btnType: {
+    type: String,
+    default: "default",
+    validator(btnType) {
+      return ["success", "warning", "danger"].includes(btnType);
+    },
+  },
+});
+</script>
