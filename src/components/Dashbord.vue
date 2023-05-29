890fef209adef0053b213af1661e83c580d09df8
alexzhang <alexzhangyaotian@gmail.com>
2023-05-29 10:46:08 +0800

    modify:动态css & ref全家桶

--- src/components/Dashbord.vue
+++ src/components/Dashbord.vue
@@ -2,15 +2,21 @@
   <NextTickT></NextTickT>
   <TeleportT></TeleportT>
   <LifeHooks></LifeHooks>
+  <DynamicCss></DynamicCss>
+  <RefFamily></RefFamily>
 </template>
 <script setup>
 import NextTickT from "./NextTickT.vue";
 import TeleportT from "./TeleportT.vue";
 import LifeHooks from "./LifeHooks.vue/index.vue";
+import DynamicCss from "./DynamicCss.vue";
+import RefFamily from "./RefFamily.vue";
 import { defineComponent } from "vue";
 defineComponent({
   NextTickT,
   TeleportT,
   LifeHooks,
+  DynamicCss,
+  RefFamily,
 });
 </script>
---
+++ src/components/DynamicCss.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>hello</p>
+</template>
+<script setup>
+import { ref } from "vue";
+const theme = ref("red");
+
+const colors = ["blue", "yellow", "red", "green"];
+
+setInterval(() => {
+  theme.value = colors[Math.floor(Math.random() * 4)];
+}, 1000);
+</script>
+<style scoped>
+/* 修改以下代码绑定动态颜色 */
+p {
+  color: v-bind(theme);
+}
+</style>
